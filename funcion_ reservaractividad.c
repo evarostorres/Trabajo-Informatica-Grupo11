d22b94194e9d1690d23ae959d6f97846db45580d
Iness1234 <inesmucha2007@gmail.com>
2026-05-05 23:03:30 +0200

    Update funcion_ reservaractividad.c

--- funcion_ reservaractividad.c
+++ funcion_ reservaractividad.c
@@ -13,80 +13,92 @@ void reservar_actividad(AnalisisDatos lista[], int n){
 	char diasemana[20];
 	char horai[10];
 	char horaf[10];
+	char tipactividad[20];
 	int i;
 	int encontrado; 
-	int opcion;
+	int opcion = 2;
+	
+
+	do{
+		encontrado = 0; // Igualamos el encontrado a 0, ya que suponemos que no ha encontrado la actividad, pero en el caso de que haya un cambio no se repite el bucle.
+		 printf("\n--- Reserva actividad ---\n");
+		
+		printf("Introduce el anio:");
+		scanf("%i", &anio);
+		
+		printf("Intrdouce el  mes(numero):");
+		scanf("%i", &mes);
+		
+		printf("Introduce el dia (numero):");
+   		scanf("%d", &dian); 
+   		
+   		printf("Introduce el dia de la semana:");
+   		scanf(" %[^\n]", diasemana); 
+   					
+		printf("Introduce la hora inicial:");
+		scanf(" %[^\n]",horai); 
+		
+		printf("Introduce la hora final:");
+		scanf(" %[^\n]",horaf); 
+		
+		printf("Introduce la actividad a la que se quiere apuntar: "); 
+		scanf(" %[^\n]", nombreactividad); 
+		
+		printf("Modalidad: ");
+		scanf(" %[^\n]", modalidad);
+		
+		printf("Introduce el centro deportivo:");
+		scanf(" %[^\n]",centro); 
+		
+		printf("Introduce el tipo de actividad:");
+		scanf(" %[^\n]",tipactividad); 
+		
+		
+
+		
+		
+		//strcmp sirve para comparar cadenas, si devulve 0,que significa que los textos son iguales. 
+		// El strcmp compara dos cadenas de caraceteres. En este caso queremos comporar los caracteres de la lista
+		// El punto es para que dentro del vector de la lista comprare cada apartado con otro
+		for(i = 0; i<n; i++){
+			if(lista[i].ano == anio && lista[i].mes == mes &&lista[i].dia == dian && strcmp(lista[i].dia_semana, diasemana)==0 &&strcmp(lista[i].hora_inicial, horai)==0
+			&&strcmp(lista[i].hora_final, horaf)==0 && strcmp(lista[i].actividad, nombreactividad)==0 && strcmp(lista[i].modalidad, modalidad)==0
+			&&strcmp(lista[i].centro_deportivo,centro)==0	&& strcmp(lista[i].tipo_actividad,tipactividad)==0){
+			
+			 	
+				encontrado=1; // Encuentra a actividad
+				
+				
+				// Ahora hay que ver si hay plazas libres
+				if(lista[i].libres > 0){ // Si es mayor a 0, es 1 por lo q hay plazas libres.
+					printf("Hay plazas. Reserva confirmada\n ");
+					lista[i].libres --;
+					lista[i].ocupadas++;
+				
+					opcion = 2; //Sirve para salir del bucle
+				
+				}else{
+					printf("No quedan plazas disponibles\n");
+					printf("Seleccione una opcion: 1 Volver a introducir los datos/ 2 Salir de la opcion reserva\n");
+					scanf("%i", &opcion); // Guarda el número en la variable opcion q hay aelegido el usuario
+					
+					
 		
 	
-		do{
-			encontrado = 0; // Igualamos el encontrado a 0, ya que suponemos que no ha encontrado la actividad, pero en el caso de que haya un cambio no se repite el bucle.
-			 printf("\n--- Reserva actividad ---\n");
-			
-			printf("Introuce el anio:",anio);
-			scanf("%i", &anio);
-			
-			printf("Introuce el  mes(numero):", mes);
-			scanf("%i", &mes);
-			
-			printf("Introduce el dia (numero):");
-       		scanf("%d", &dian); 
-       		
-       		printf("Introduce el dia de la semana:");
-       		scanf(" %[^\n]", diasemana); 
-       					
-			printf("Introduce la hora inicial:");
-			scanf(" %[^\n]",horai); 
-			
-			printf("Introduce la hora final:");
-			scanf(" %[^\n]",horaf); 
-			
-			printf("Introduce la actividad a la que se quiere apuntar: "); 
-			scanf(" %[^\n]", nombreactividad); 
-			
-			printf("Modalidad: ");
-   			scanf(" %[^\n]", modalidad);
-   			
-   			printf("Introduce el centro deportivo:");
-			scanf(" %[^\n]",centro); 
-			
-
-			
-			
-			//strcmp sirve para comparar cadenas, si devulve 0,que significa que los textos son iguales. 
-			// El strcmp compara dos cadenas de caraceteres. En este caso queremos comporar los caracteres de la lista
-			// El punto es para que dentro del vector de la lista comprare cada apartado con otro
-				for(i = 0; i<n; i++){
-					if(lista[i].ano == anio&&lista[i].mes == mes &&lista[i].dia == dian &&strcmp(lista[i].dia_semana, diasemana)==0 &&strcmp(lista[i].hora_inicial, horai)==0
-					&&strcmp(lista[i].hora_final, horaf)==0 && strcmp(lista[i].actividad, nombreactividad)==0 && strcmp(lista[i].tipo_actividad, modalidad)==0
-					&&strcmp(lista[i].centro_deportivo,centro)==0){
+				}
+					break; //salimos del for
 					
-					 	
-						encontrado=1; // Encuentra a actividad
-						
-						
-						// Ahora hay que ver si hay plazas libres
-						if(lista[i].libres > 0){ // Si es mayor a 0, es 1 por lo q hay plazas libres.
-							printf("Hay plazas. Reserva confirmada\n ");
-							lista[i].libres --;
-							lista[i].ocupadas++;
-						}else{
-							printf("No quedan plazas disponibles\n");
-							printf("Seleccione una opcion: 1 Volver a introducir los datos/ 2 Salir de la opción reserva");
-							scanf("%i", &opcion); // Guarda el número en la variable opcion q hay aelegido el usuario
-							
-							if(opcion == 1){
-								encontrado = 0; // Se repite el bucle. Si fuese encontrado=1 no se repite el bucle 
-							}
-				
-			
-						}
-						break; //salimos del for
-						
-					}
+			}
+		}
+	
+		if(encontrado == 0){ //Si no la encuentra entones te da las opciones, si seleccionas 
+            printf("No quedan existe la actividad que desea reservar\n");
+			printf("Seleccione una opcion: 1 Volver a introducir los datos/ 2 Salir de la opcion reserva\n");
+			scanf("%i", &opcion);
 		}
 		
-	}while(opcion==1); //Si encontrado es 0, es decir se cumeple que cuando enontrado = 0 si se repite el bucle
-	
+	}while(opcion==1); //Si encontrado es 0, es decir se cumeple que cuando enontrado = 0 si se repite el bucle. Si el usuario introduce el 1 se repite el bucle
+	//Si elige la opción 1, entonces es verdadera la condición y se vuelve a ejecutar el bucle
 }
 //Prototipo void reservar_actividad(AnalisisDatos lista[], int n);
 //Llamda  		reservar_actividad(lista, n);
