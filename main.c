292bf974178d2e225fb84ae88d8c8e2d815cf5d4
evarostorres <evarostorres@gmail.com>
2026-05-08 17:47:53 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <wchar.h>
 #include "pbPlots.h"
 #include "supportLib.h"
-#include "estructProtot.h"
 
 #define MAX_LONG 20
 
 int main(){
+	
 	int opcionAcceso;
 	int InSesionExitoso = 0;	// Bandera para saber si podemos pasar al menú principal
 	char usuarioLogueado[MAX_LONG];
+	
 	int opcionMenu;
-	int datos, actividades;
+	
 	AnalisisDatos *datos;
-	int num_datos;
-	char nombre_centro[100];
-	Centro centro_usuario;
+	
+	char centros[100][100];
+	int num_centros;
+	
+    char nombre_centro[100];
+    Centro centro_usuario;
+	
+	char opcion;
 	
 	// Iniciamos sesión o nos registramos antes de entrar al menú (sistema de acceso)
     do {
@@ -45,8 +52,7 @@ int main(){
 				printf("Opcion no valida.\n");
         }
     } while (opcionAcceso != 3);
-
-
+    
     //Se abre y se lee el fichero de datos
     int n = 0;
     AnalisisDatos *lista = NULL;
@@ -57,7 +63,6 @@ int main(){
         printf("Error: No se pudo cargar el archivo o la memoria.\n");
         return -1;
     }
-
     
     if (InSesionExitoso) {
 		// Menú principal con bucle y switch (sistema de gestión)
@@ -74,23 +79,68 @@ int main(){
 	        printf("\nSeleccione una opcion: ");
 	        
 	        scanf("%d", &opcionMenu);
-	
-	        switch (opcionMenu) {
-	            case 1:
-					crear_centro_por_nombre(nombre_centro, datos, num_datos);	// Llamada a la función
-					mostrar_actividades_centro(&centro_usuario);
-					//Falta copiar la introducion a la funcion añadir favoritos y la funcion
+	        
+			switch (opcionMenu) {
+				case 1:
+					//Crear estructura Centro a partir de los datos
+				    centro_usuario = crear_centro_por_nombre(nombre_centro, lista, n);
+				
+					//Mostramos al usuario la lista de centros 
+					printf("\nLista de centros disponibles:\n");
+					
+					num_centros = mostrar_centros(lista, n, centros);
+					
+					
+					printf("\nElige un centro (numero): ");
+					scanf(" %d", &opcion);
+				
+					
+					//Comprobación de si la opción es válida
+					if (opcion < 1 || opcion > num_centros)
+					{
+					    printf("Opcion no valida\n");
+					    return 1;
+					}
+				
+					strcpy(nombre_centro, centros[opcion - 1]);
+					
+					//Agrupamos las actividades por centro
+					centro_usuario = crear_centro_por_nombre(nombre_centro, lista, n);
+				
+				
+				    //Mostrar actividades
+				    mostrar_actividades_centro(&centro_usuario);
+					
+					char opcionAnadirFav;
+					printf("\nQuieres anadir alguna actividad a favoritos? (s/n): ");
+					scanf(" %c", &opcionAnadirFav);
+					
+					if (opcionAnadirFav == 's' || opcionAnadirFav == 'S')
+					{
+					    anadir_favorito("favoritos.txt", usuarioLogueado, &centro_usuario);
+					}
+					
+					//Opción de ver favoritos (si existan o no)
+					char opcionVerFav;
+					printf("\nQuieres ver tus favoritos? (s/n): ");
+					scanf(" %c", &opcionVerFav);
+					if (opcionVerFav == 's' || opcionVerFav == 'S'){
+						ver_favoritos("favoritos.txt", usuarioLogueado);
+					}
 	                break;
-	            case 2:
+				case 2:
 	                reservar_actividad(lista, n); // Llamada a la función
 	                break;
-	            case 3:
-	                ver_frecuencia(datos, actividades);     // Llamada a la función
+				case 3:
+	                //Mostramos al usuario la lista de centros 
+					printf("\nLista de centros disponibles:\n");
+					num_centros = mostrar_centros(lista, n, centros);
+					
+					ver_frecuencia(lista, n);     // Llamada a la función
 					break;
 				case 4:
-	                ver_graficas_centros(datos, actividades);	// Llamada a la función
-					break;
-	            case 5:
+	                ver_graficas_centros(lista, n);	// Llamada a la función
+					break;case 5:
 	                ver_favoritos("favoritos.txt", usuarioLogueado);	// Llamada a la función
 	                break;
 	            case 0:
@@ -100,13 +150,14 @@ int main(){
 	                printf("Opcion no valida. Vuelve a intentarlo.\n");
 	        }
 			
-			//Liberamos memoria
-			free(centro_usuario.lista_actividades);
-			free(datos);
-			
 	    } while (opcionMenu != 0); // El bucle se repite mientras no se pulse 0
 	
 	    return 0;
 	}
+	
+	//Liberamos memoria 
+	free(centro_usuario.lista_actividades);
+	free(lista);
+	
+	return 0;
 }
-    
