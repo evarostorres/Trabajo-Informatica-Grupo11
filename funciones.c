91f7b33638aeecb579b15782cce1c29680ba2fda
alba-01 <carmen.rosalesmontana@gmail.com>
2026-05-09 19:22:10 +0200

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -173,7 +173,8 @@ AnalisisDatos *lectura_fichero(const char* nombrearchivo, int* n)
 }
 
 
-Centro crear_centro_por_nombre(const char *nombre, AnalisisDatos *datos, int num_datos){
+Centro crear_centro_por_nombre(const char *nombre, AnalisisDatos *datos, int num_datos)
+{
 	
 	Centro c;
     int i;
@@ -260,35 +261,42 @@ int mostrar_centros(AnalisisDatos *datos, int num_datos, char centros[][100])
 }
 
 
-void mostrar_actividades_centro(const Centro *c){
+void mostrar_actividades_centro(const Centro *c)
+{
 	
     int i;
 
+	// Comprueba que se ha elegido una opción válida
     if (c == NULL)
     {
         printf("Centro no valido\n");
         return;
     }
 
+	// Imprime el nombre del centro seleccionado
     printf("Centro deportivo: %s\n", c->nombre);
     printf("---------------------------------\n");
 
+	// Si el centro no tiene actividades
     if (c->num_actividades == 0)
     {
         printf("Este centro no tiene actividades disponibles\n");
         return;
     }
-
+	
+	// Si el centro tiene actividades
     for (i = 0; i < c->num_actividades; i++)
     {
         AnalisisDatos acti = c->lista_actividades[i];
 
         printf("Actividad: %s\n", acti.actividad);
 
+		// Comprueba si la actividad es de tipo dirigida
         if (strcmp(acti.tipo_actividad, "actividad_dirigida") == 0)
         {
             printf("Tipo: Actividad dirigida\n");
         }
+		// Comprueba si la actividad es de tipo uso libre
         else if (strcmp(acti.tipo_actividad, "uso_libre") == 0)
         {
             printf("Tipo: Uso libre\n");
@@ -298,15 +306,18 @@ void mostrar_actividades_centro(const Centro *c){
             printf("Tipo: %s\n", acti.tipo_actividad);
         }
 
+		// Imprime la hora, las plazas libres y ocupadas 
         printf("Horario: %s - %s\n", acti.hora_inicial, acti.hora_final);
         printf("Plazas totales: %d\n", acti.plazas);
         printf("Plazas ocupadas: %d\n", acti.ocupadas);
         printf("Plazas libres: %d\n", acti.libres);
 
+		// Si no hay actividades libres impreme que el estado es completo
         if (acti.libres == 0)
 		{
 		    printf("Estado: Completo\n");
 		}
+		// Si hay actividades entonces imprime que hay plazas disponibles
 		else
 		{
 		    printf("Estado: Plazas disponibles\n");
@@ -317,7 +328,8 @@ void mostrar_actividades_centro(const Centro *c){
 }
 
 
-void reservar_actividad(AnalisisDatos lista[], int n){
+void reservar_actividad(AnalisisDatos lista[], int n)
+{
   	char centros[100][100];
     char nombre_centro[100];
 
@@ -329,98 +341,126 @@ void reservar_actividad(AnalisisDatos lista[], int n){
 	int i;
 	int salir = 0;
 
-	//LLAMADA FUNCION 1
+	// LLAMADA FUNCION 1
 	num_centros = mostrar_centros(lista, n, centros);
 	printf("\n-------Lista de centros -------\n");
 
-	do{ // Este do while sirve para q lo vuelva a pedir hasta q el usuario introduzca el número correcto
-	
+	// Este do while sirve para q lo vuelva a pedir hasta q el usuario introduzca el número correcto
+	do
+	{ 
 		
 		printf("Selecciona un numero del centro donde se quiere apuntar(1-%d): ", num_centros);
-		scanf("%i", &opcion_centro); //Aqui el usuario introduce la posición, del centro.
+		//Aqui el usuario introduce la posición, del centro.
+		scanf("%i", &opcion_centro); 
 		
-	}while(opcion_centro<1 || opcion_centro>num_centros);
-	strcpy(nombre_centro, centros[opcion_centro - 1]);	 // En el 1 argumento le restamos 1, pq es una cadena	
+	}
+	while (opcion_centro<1 || opcion_centro>num_centros);
+	// En el primer argumento le restamos 1 porque es una cadena
+	strcpy(nombre_centro, centros[opcion_centro - 1]);		
 	
-	//LLAMADA FUNCION 2
+	// LLAMADA A FUNCIÓN 2
 	Centro_usuario = crear_centro_por_nombre(nombre_centro, lista, n);
 
+	// Imprimios la lista de actividades
 	printf("\n-------Lista de actividades por centro -------\n");
-	// imprimios la lista de actividades
-	for(i = 0; i<Centro_usuario.num_actividades; i++){ // El punto sirve para acceder a la estructura
-		printf("%d. %s || %s - %s || Libres: %d\n", i + 1, // Se pone i + 1 pq el ususario ve eso, ya que si introduce 1, la posición es 0
+
+	// El punto sirve para acceder a la estructura
+	for (i = 0; i<Centro_usuario.num_actividades; i++)
+	{ 
+		// Se pone i + 1 porque el ususario ve eso, ya que si introduce 1, la posición es 0
+		printf("%d. %s || %s - %s || Libres: %d\n", i + 1, 
     	Centro_usuario.lista_actividades[i].actividad,
     	Centro_usuario.lista_actividades[i].hora_inicial,
         Centro_usuario.lista_actividades[i].hora_final,
    		Centro_usuario.lista_actividades[i].libres);
 		
 	}
-	//Selección de la  actividad q desea reservar
-	do{
+	// Selección de la  actividad q desea reservar
+	do
+	{
 		printf("Selecciona un numero de la actividad a la que se quiere apuntar ", num_centros);
 		scanf("%i", &opcion_act);	
-	}while(opcion_act<1 || opcion_act > Centro_usuario.num_actividades);
+	}
+		while (opcion_act<1 || opcion_act > Centro_usuario.num_actividades);
 	
 	opcion_act--;
 	
-	while(salir ==0){
+	while(salir ==0)
+	{
 
-	//Ahora reservamos la actividad
-	if(Centro_usuario.lista_actividades[opcion_act].libres > 0){
-		for(i = 0; i<n; i++){
-			if(strcmp(lista[i].centro_deportivo, Centro_usuario.nombre)==0 && strcmp(lista[i].actividad,Centro_usuario.lista_actividades[opcion_act].actividad)==0
-			&& strcmp(lista[i].hora_inicial,Centro_usuario.lista_actividades[opcion_act].hora_inicial)== 0){
-				
-				lista[i].ocupadas++;
-                lista[i].libres--;
-                printf("RESERVA CONFIRMADA\n");
-                
-                salir = 1; 
-                
-                break;
-				
+		// Ahora reservamos la actividad
+		if (Centro_usuario.lista_actividades[opcion_act].libres > 0)
+		{
+			for (i = 0; i<n; i++)
+			{
+				if(strcmp(lista[i].centro_deportivo, Centro_usuario.nombre)==0 && strcmp(lista[i].actividad, 
+					Centro_usuario.lista_actividades[opcion_act].actividad)==0 
+					&& strcmp(lista[i].hora_inicial,Centro_usuario.lista_actividades[opcion_act].hora_inicial)== 0)
+				{
+					
+					lista[i].ocupadas++;
+	                lista[i].libres--;
+	                printf("RESERVA CONFIRMADA\n");
+	                
+	                salir = 1; 
+	                break;
+					
+				}
 			}
 		}
-	//En el caso q no haya plazas
-	}else{
-		int opcion;
-		int encontrada = 0;
-		printf("No hay plazas disponibles\n");
-		printf("SUGERENCIAS DENTRO DEL CENTRO CON LOS MISMOS HORARIOS DE LA ACTIVIDAD SELECCIONADA:\n");
-		
-		
-		for(i = 0; i<Centro_usuario.num_actividades; i++){
-			if(strcmp(Centro_usuario.lista_actividades[i].hora_inicial, Centro_usuario.lista_actividades[opcion_act].hora_inicial)==0
-			&&strcmp(Centro_usuario.lista_actividades[i].hora_final, Centro_usuario.lista_actividades[opcion_act].hora_final)==0
-			&& Centro_usuario.lista_actividades[i].libres>0 && i != opcion_act){ 
-				printf("%d. %s || %s - %s || Libres: %d\n", i + 1, // Se pone i + 1 pq el ususario ve eso
-		    	Centro_usuario.lista_actividades[i].actividad,
-		    	Centro_usuario.lista_actividades[i].hora_inicial,
-		        Centro_usuario.lista_actividades[i].hora_final,
-		   		Centro_usuario.lista_actividades[i].libres);
+			
+		//En el caso q no haya plazas
+		else
+		{
+			int opcion;
+			int encontrada = 0;
+			
+			printf("No hay plazas disponibles\n");
+			printf("SUGERENCIAS DENTRO DEL CENTRO CON LOS MISMOS HORARIOS DE LA ACTIVIDAD SELECCIONADA:\n");
+			
+			
+			for (i = 0; i<Centro_usuario.num_actividades; i++)
+			{
+				if (strcmp(Centro_usuario.lista_actividades[i].hora_inicial, Centro_usuario.lista_actividades[opcion_act].hora_inicial)==0
+				&&strcmp(Centro_usuario.lista_actividades[i].hora_final, Centro_usuario.lista_actividades[opcion_act].hora_final)==0
+				&& Centro_usuario.lista_actividades[i].libres>0 && i != opcion_act)
+				{ 
+					// Se pone i + 1 porque el ususario ve eso
+					printf("%d. %s || %s - %s || Libres: %d\n", i + 1,
+			    	Centro_usuario.lista_actividades[i].actividad,
+			    	Centro_usuario.lista_actividades[i].hora_inicial,
+			        Centro_usuario.lista_actividades[i].hora_final,
+			   		Centro_usuario.lista_actividades[i].libres);
+					
+					encontrada = 1;
+				}
 				
-				encontrada = 1;
-				
+			} 
+			
+			if (encontrada ==0)
+			{
+				printf("NO quedan actividades a esa hora\n");
 			}
 			
-		} 
-		if(encontrada ==0){
-			printf("NO quedan actividades a esa hora\n");
-		}
-		printf("1. Volver a intentar: \n");
-		printf("2. Salir: \n");
-	
-		scanf("%d", &opcion); 
-			if(opcion == 2){
-				salir = 1;
-			}else{
-				do{
-				printf("Selecciona otra actividad: ");
-           			scanf("%d", &opcion_act);
-				}while(opcion_act < 1 || opcion_act > Centro_usuario.num_actividades);
-				opcion_act--;
-			}
-		}		
+			printf("1. Volver a intentar: \n");
+			printf("2. Salir: \n");
+			scanf("%d", &opcion); 
+			
+				if (opcion == 2)
+				{
+					salir = 1;
+				}
+				else
+				{
+					do
+					{
+						printf("Selecciona otra actividad: ");
+	           			scanf("%d", &opcion_act);
+					}
+					while (opcion_act < 1 || opcion_act > Centro_usuario.num_actividades);
+					opcion_act--;
+				}
+			}		
 	}
 }
 
